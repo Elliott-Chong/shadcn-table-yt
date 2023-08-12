f1abe6fc9eb4f520747a61c3d08749128cefb905
Elliott Chong <elliottchong16@gmail.com>
2023-08-12 14:10:14 +0800

    add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-
+import {redirect} from 'next/navigation'
 export default function Home() {
-  return <Button>Hello!</Button>;
+  return redirect('/people');
 }
